2f03682f30c4548e0ea4ef3abd0608a43f5d7b62
renanrgs <renan.rgsilva@gmail.com>
2017-11-21 09:03:06 -0200

    Stream Test

---
+++ ocp-chapter8/src/test/stream/TestStream.java
@@ -0,0 +1,15 @@
+package test.stream;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TestStream {
+	public static void main(String[] args) throws IOException {
+		FileReader fileReader = new FileReader(new File("files/test.txt"));
+		BufferedReader b = new BufferedReader(fileReader);
+		
+		System.out.println(b.readLine());
+	}
+}
