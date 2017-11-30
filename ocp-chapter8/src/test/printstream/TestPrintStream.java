ccdb14cc932e03dd643a99c02a7770d71aadbb04
renanrgs <renan.rgsilva@gmail.com>
2017-11-30 08:36:17 -0200

    intellij modifications

---
+++ ocp-chapter8/src/test/printstream/TestPrintStream.java
@@ -0,0 +1,13 @@
+package test.printstream;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class TestPrintStream {
+	public static void main(String[] args) throws FileNotFoundException {
+		try (PrintStream printStream = new PrintStream(new File("files/maven_renan"))) {
+			printStream.println("Renan Guedes da Silva");
+		}
+	}
+}
