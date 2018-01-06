9d50214a873217e25856badf57e2ac79948f495c
Renan <renan.rgsilva@gmail.com>
2018-01-06 09:12:27 -0200

    Testing ForkJoin

---
+++ concurrent-processes/src/forkjoinpool/ForkJoinFramework.java
@@ -0,0 +1,15 @@
+package forkjoinpool;
+
+import java.util.concurrent.RecursiveAction;
+
+public class ForkJoinFramework extends RecursiveAction{
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	protected void compute() {
+		
+	}
+	
+}
